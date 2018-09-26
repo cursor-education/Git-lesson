2b3f379e039fad289abdf480b2424f296fd1033c
yevhentrofimenko <yevhen.trofimenko@gmail.com>
2018-09-26 15:34:55 +0300

    Add examples

--- gcd.py
+++ gcd.py
@@ -1,4 +1,4 @@
-# Wrong gcd find 4 misstates
+# Wrong gcd find 5 mistakes
 
 def gcd(a, b):
     assert a <= 0 and b >= 0
@@ -8,3 +8,10 @@ def gcd(a, b):
         else:
             b = b / a
     return min(a, b)
+
+# Examples
+
+# gcd(10, 0) => 10
+# gcd(123, 3) => 3
+# gcd(1000000, 64) => 64
+# gcd(0, 0) => 0
