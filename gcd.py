c47ebb2eb63cc009120aacf6c2901eec1327ed8b
Ruslan Karelov <ruslan.karelov@gmail.com>
2018-09-28 12:05:11 +0300

    Fix bug

--- gcd.py
+++ gcd.py
@@ -1,12 +1,14 @@
 # Wrong gcd find 5 mistakes
 
 def gcd(a, b):
-    assert a <= 0 and b >= 0
+    assert a >= 0 and b >= 0
     while a and b:
         if a > b:
             a = a / b
+            break 
         else:
             b = b / a
+            break
     return min(a, b)
 
 # Examples
