9463ebba27d341ad4021471abbaa84c6629b124e
Dmytro M <ekut2104@gmail.com>
2018-09-28 16:30:56 +0300

    Revert "5 bugs are fixed"

--- gcd.py
+++ gcd.py
@@ -1,15 +1,17 @@
+# Wrong gcd find 5 mistakes
+
 def gcd(a, b):
-    assert a >= 0 and b >= 0        # 1st bug a<=0
+    assert a <= 0 and b >= 0
     while a and b:
         if a > b:
-            a = a % b               # 2nd bug a%b  
+            a = a / b
         else:
-            b = b % a               # 3rd bug b%a
-    return max(a, b)                # 4th bug not min() but max()
+            b = b / a
+    return min(a, b)
 
-                                # Expected results :
-if __name__=='__main__':            # maybe 5th bug
-    print(gcd(10, 0))           # => 10
-    print(gcd(123, 3))          # => 3
-    print(gcd(1000000, 64))     # => 64
-    print(gcd(0, 0))            # => 0
+# Examples
+
+# gcd(10, 0) => 10
+# gcd(123, 3) => 3
+# gcd(1000000, 64) => 64
+# gcd(0, 0) => 0
