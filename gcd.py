b628429379e877b9049ea58e3ea3707090fd5068
RomanBuryy <roman.buriy@gmail.com>
2018-09-28 09:17:49 +0300

    Bugfix Roman Buryy

--- gcd.py
+++ gcd.py
@@ -1,14 +1,26 @@
 # Wrong gcd find 5 mistakes
 
 def gcd(a, b):
-    assert a <= 0 and b >= 0
-    while a and b:
-        if a > b:
-            a = a / b
-        else:
-            b = b / a
-    return min(a, b)
+    assert a >= 0 and b >= 0
 
+    if a == 0:
+        return b
+    
+    if b == 0:
+        return a
+
+    while a != b:
+        if a > b:
+            a = a - b
+        else:
+            b = b - a
+    
+    return a
+
+
+
+
+print(gcd(10, 0))
 # Examples
 
 # gcd(10, 0) => 10
