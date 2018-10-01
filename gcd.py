5328dcef48669ac5980deb0d45afbc2f56f63d76
dreasty <dreasty,d@gmail.com>
2018-10-01 09:52:21 +0300

    Fixed bugs in gdc

--- gcd.py
+++ gcd.py
@@ -1,13 +1,13 @@
 # Wrong gcd find 5 mistakes
 
 def gcd(a, b):
-    assert a <= 0 and b >= 0
+    assert a >= 0 and b >= 0
     while a and b:
         if a > b:
-            a = a / b
+            a = a % b
         else:
-            b = b / a
-    return min(a, b)
+            b = b % a
+    return a + b
 
 # Examples
 
