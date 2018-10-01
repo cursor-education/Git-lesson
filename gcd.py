b369f14ea9230f26895d92d2ce57584cc0a6f7db
remko740 <remko740@gmail.com>
2018-10-01 18:22:59 +0300

    My changes for 4 task

--- gcd.py
+++ gcd.py
@@ -1,17 +1,14 @@
-# Wrong gcd find 5 mistakes
 
-def gcd(a, b):
-    assert a <= 0 and b >= 0
+def gcd(a:int, b:int):
+    assert a >= 0 and b >= 0
     while a and b:
         if a > b:
-            a = a / b
+            a = a % b
         else:
-            b = b / a
-    return min(a, b)
+            b = b % a
+    return max(a, b)
 
-# Examples
-
-# gcd(10, 0) => 10
-# gcd(123, 3) => 3
-# gcd(1000000, 64) => 64
-# gcd(0, 0) => 0
+print(gcd(10, 0))
+print(gcd(123, 3))
+print(gcd(1000000, 64))
+print(gcd(0, 0))
