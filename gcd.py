afc04ec26dcf128c03fecb8946f8276a07012ca5
Bozon Higgsa <bozonmarketolog@gmail.com>
2018-10-02 01:13:57 +0300

    fix all 5 mistakes

--- gcd.py
+++ gcd.py
@@ -1,15 +1,20 @@
-# Wrong gcd find 5 mistakes
+# Wrong gcd find 5 mistakes <- maybe this is fifth mistakes?
 
 def gcd(a, b):
-    assert a <= 0 and b >= 0
-    while a and b:
+    assert a >= 0 and b >= 0
+    while a != 0 and b != 0:
         if a > b:
-            a = a / b
+            a = a % b
         else:
-            b = b / a
-    return min(a, b)
+            b = b % a
+    return max(a, b)
 
-# Examples
+
+print(gcd(10, 0))
+print(gcd(123, 3))
+print(gcd(1000000, 64))
+print(gcd(0, 0))
+
 
 # gcd(10, 0) => 10
 # gcd(123, 3) => 3
