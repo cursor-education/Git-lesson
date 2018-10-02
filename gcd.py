acaa21f06981fa8a19d428598a99d1df8a04441e
Volodymyr Lashko <43449563+BobCursor@users.noreply.github.com>
2018-10-02 17:47:01 +0300

    Change in line 7, returns remainder after divide

--- gcd.py
+++ gcd.py
@@ -4,7 +4,7 @@ def gcd(a, b):
     assert a >= 0 and b >= 0
     while a != 0 and b != 0:
         if a > b:
-            a = a / b
+            a = a % b
         else:
             b = b / a
     return min(a, b)
