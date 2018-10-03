38cfc36a25283da8e38a91f1b703b4abdc0da98c
Maxim-Poleshko <poleshkomaxim@gmail.com>
2018-10-03 22:47:59 +0300

    Fix 5 bugs

--- gcd.py
+++ gcd.py
@@ -1,17 +1,15 @@
 # Wrong gcd find 5 mistakes
-
 def gcd(a, b):
-    assert a <= 0 and b >= 0
+    assert a >= 0 and b >= 0    #1st bug: a <= 0
     while a and b:
         if a > b:
-            a = a / b
+            a = a % b           #2nd bug: a / b
         else:
-            b = b / a
-    return min(a, b)
+            b = b % a           #3rd bug: b / a
+    return max(a, b)            #4th bug: min()
 
-# Examples
+print(gcd(10, 0))               #5th bug: The programm doesn't have print() function
+print(gcd(123, 3))
+print(gcd(1000000, 64))
+print(gcd(0, 0))
 
-# gcd(10, 0) => 10
-# gcd(123, 3) => 3
-# gcd(1000000, 64) => 64
-# gcd(0, 0) => 0
