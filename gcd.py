fab450967bd26709a4babeebbb2bf9cb85227b65
Roman Ponomaryov <infinitywarden@gmail.com>
2018-10-04 13:23:56 +0300

    Fixes after the peer review pointed out some mistakes

--- gcd.py
+++ gcd.py
@@ -3,12 +3,12 @@
 def gcd(a, b): # mistake 1 - we do not specify explicitly that the type must be int
     assert a <= 0 and b >= 0  # mistake 2 - a will be asserted to be *less* than 0 in this case;
     # mistake 3 - there is no error message in case assertion fails - it is optional, but still better to have it
-    while a and b: # mistake 4 - eternal loop if initially a or b is not 0
-        if a > b: # mistake 5 - risk of dividing by zero if b = 0
-            a = a / b
-        else: # same mistake 5 if a =0
-            b = b / a
-    return min(a, b)
+    while a and b:
+        if a > b:
+            a = a / b  # mistake 4 - we have to use modulo in order to know both the remainder and to escape the loop once the remainder would be 0
+        else:
+            b = b / a  # same mistake 4
+    return min(a, b)  # mistake 5 - should be max, as we are looking for the *greatest* divisor
 
 # Examples
 
--- gcd_fixed.py
+++ gcd_fixed.py
@@ -1,27 +1,14 @@
-# Wrong gcd find 5 mistakes
-
 def gcd(a:int, b:int):
     assert a >= 0 and b >= 0, "Please do not use negative integers"
-    remainder = 1
-    while remainder:
-        if a == 0 or b == 0:
-            return max(a,b)
-        elif a > b:
-            remainder = a % b
-            a = a / b
+    while a and b:
+        if a > b:
+            a = a % b
         else:
-            b = b / a
-            remainder = b % a
-    return min(a, b)
-
-# Examples
-
-# gcd(10, 0) => 10
-# gcd(123, 3) => 3
-# gcd(1000000, 64) => 64
-# gcd(0, 0) => 0
+            b = b % a
+    return max(a, b)
 
 print(gcd(10, 0))
 print(gcd(123, 3))
 print(gcd(1000000, 64))
 print(gcd(0, 0))
+print(gcd(32, 64))
