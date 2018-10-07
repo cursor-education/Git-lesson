d3b7eeb6a1c1bc8fc353798c22b6d9033144f4fb
Anastasiia Kostenko <kostenko.nastasiia@gmail.com>
2018-10-07 20:28:55 +0300

    All bugs were fixed

--- gcd.py
+++ gcd.py
@@ -1,17 +1,17 @@
 # Wrong gcd find 5 mistakes
 
 def gcd(a, b):
-    assert a <= 0 and b >= 0
-    while a and b:
+    assert a >= 0 and b >= 0
+    while a!=0 and b!=0:
         if a > b:
-            a = a / b
+            a = a % b
         else:
-            b = b / a
-    return min(a, b)
+            b = b % a
+    return a+b
 
 # Examples
 
-# gcd(10, 0) => 10
-# gcd(123, 3) => 3
-# gcd(1000000, 64) => 64
-# gcd(0, 0) => 0
+print(gcd(10, 0))
+print(gcd(123, 3))
+print(gcd(1000000, 64))
+print((0, 0))
